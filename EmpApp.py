0e47a17d9254318322fafa18979e874aa611a398
KaiYit123 <86282989+KaiYit123@users.noreply.github.com>
2022-04-08 02:02:53 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -23,7 +23,7 @@ table = 'employee'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    return render_template('Homepage.html')
 
 
 @app.route("/add")
