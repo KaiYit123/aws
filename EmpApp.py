24b2d9bf15c02050bf9d2f75566168a46b26552e
KaiYit123 <86282989+KaiYit123@users.noreply.github.com>
2022-04-08 14:49:02 +0800

    Add files via upload

--- EmpApp.py
+++ EmpApp.py
@@ -62,9 +62,9 @@ def update():
     pri_skill = request.form['pri_skill']
     location = request.form['location']
 
-    rtr_sql = "UPDATE employee SET first_name=%s, last_name=%s, pri_skill=%s,location=%s WHERE emp_id = %s"
+    update_sql = "UPDATE employee SET first_name=%s, last_name=%s, pri_skill=%s,location=%s WHERE emp_id = %s"
     cursor = db_conn.cursor()
-    cursor.execute(rtr_sql,(first_name,last_name,pri_skill,location,emp_id))
+    cursor.execute(update_sql,(first_name,last_name,pri_skill,location,emp_id))
     db_conn.commit()
     user = cursor.fetchone()
     cursor.close()
